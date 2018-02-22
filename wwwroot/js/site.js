8bc4fd31e028d488d90698b4a769655d0d79fc93
IAmMonmoy <iammonmoy@gmail.com>
2018-02-23 01:35:56 +0600

    added dynamic phone number add to the design

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,9 @@
 ﻿// Write your JavaScript code.
+$( document ).ready(function(){
+    $('#addPhone').on('click', addPhoneNumber);
+});
+
+function addPhoneNumber()
+{
+    $('.staticFormContent').append('<div class="form-group"><div class="row"><div class="col-md-10"><input class="form-control" id="phoneNumber" placeholder="Phone Number" type="text"></div></div></div>');
+}
