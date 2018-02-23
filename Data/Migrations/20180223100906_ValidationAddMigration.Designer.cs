9e07512d77b6331c2b01282c997eeba8325b0d7c
IAmMonmoy <iammonmoy@gmail.com>
2018-02-23 16:10:42 +0600

    added new migration for Altered column for validation

--- Data/Migrations/20180223100906_ValidationAddMigration.Designer.cs
+++ Data/Migrations/20180223100906_ValidationAddMigration.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace ContactList.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180223100906_ValidationAddMigration")]
+    partial class ValidationAddMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20180223100906_ValidationAddMigration.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ContactList.Data.Migrations
+{
+    public partial class ValidationAddMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "PhoneNumbers",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NickName",
+                table: "Person",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Person",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "PhoneNumbers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NickName",
+                table: "Person",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Person",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+    }
+}
