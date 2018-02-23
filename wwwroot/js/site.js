6957b884a63cb72633be9db3eb02f16addf53be0
IAmMonmoy <iammonmoy@gmail.com>
2018-02-23 16:42:58 +0600

    removed some bug from dynamically generated phone input

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,11 @@
 ﻿// Write your JavaScript code.
+var i = 0;
 $( document ).ready(function(){
     $('#addPhone').on('click', addPhoneNumber);
 });
 
 function addPhoneNumber()
 {
-    $('.staticFormContent').append('<div class="form-group"><div class="row"><div class="col-md-10"><input asp-for="Phones[].Phone" class="form-control" placeholder="Phone Number" type="text"></div></div></div>');
+    i++;
+    $('.staticFormContent').append('<div class="form-group"><div class="row"><div class="col-md-10"><input name="Phones['+i+'].Phone" class="form-control phoneNumber" placeholder="Phone Number" type="text"></div></div></div>');
 }
