a2b7fd99e03702128ab419f3f4d0765b2ab86ca2
IAmMonmoy <iammonmoy@gmail.com>
2018-02-23 16:59:17 +0600

    written code to ContactListController to receive form data from form

--- Controllers/ContactListController.cs
+++ Controllers/ContactListController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContactList.Models;
+using ContactList.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ContactList.Controllers
 {
@@ -15,9 +17,18 @@ namespace ContactList.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult ContactCreate(ContactFormViewModel contactForm)
+        {
+            return Json(contactForm);
+            //return View();
         }
     }
 }
